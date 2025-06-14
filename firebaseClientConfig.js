d31d4ffc1f384fd21323bfe286f1e120c97de595
Anirudh Venkatesan <1991anirudh@gmail.com>
2025-06-14 19:10:02 +0530

    Update firebaseClientConfig.js

--- firebaseClientConfig.js
+++ firebaseClientConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  apiKey: process.env.FIREBASE_API_KEY || '',
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || '',
-  projectId: process.env.FIREBASE_PROJECT_ID || '',
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || '',
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '',
-  appId: process.env.FIREBASE_APP_ID || '',
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID || '',
+  apiKey: process.env.FIREBASE_API_KEY || 'AIzaSyBgh9RutpTDCq12QYjpnFq5B7W2PAf4X14',
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN || 'pixelate-app-e5126.firebaseapp.com',
+  projectId: process.env.FIREBASE_PROJECT_ID || 'pixelate-app-e5126',
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || 'pixelate-app-e5126.appspot.com',
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '880725228002',
+  appId: process.env.FIREBASE_APP_ID || '1:880725228002:web:6f916dddcf955c24daa1bb',
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID || 'G-V4KRGLXZ1W',
 };
