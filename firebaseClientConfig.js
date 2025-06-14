801690e1f821ec4ac49a3a3643da17c1730be439
Anirudh Venkatesan <1991anirudh@gmail.com>
2025-06-14 18:57:49 +0530

    Update firebaseClientConfig.js

--- firebaseClientConfig.js
+++ firebaseClientConfig.js
@@ -1,9 +1,11 @@
-module.exports = {
-  apiKey: process.env.FIREBASE_API_KEY || '',
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || '',
-  projectId: process.env.FIREBASE_PROJECT_ID || '',
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || '',
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '',
-  appId: process.env.FIREBASE_APP_ID || '',
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID || '',
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBgh9RutpTDCq12QYjpnFq5B7W2PAf4X14",
+  authDomain: "pixelate-app-e5126.firebaseapp.com",
+  databaseURL: "https://pixelate-app-e5126-default-rtdb.firebaseio.com",
+  projectId: "pixelate-app-e5126",
+  storageBucket: "pixelate-app-e5126.appspot.com",
+  messagingSenderId: "880725228002",
+  appId: "1:880725228002:web:6f916dddcf955c24daa1bb",
+  measurementId: "G-V4KRGLXZ1W"
 };
